841ab23ea39aac4f099084dc233a92c9758403fa
betchi <shinichi.minobe@gmail.com>
2018-03-27 23:57:47 +0900

    Update PrintStruct

--- utils/print.go
+++ utils/print.go
@@ -26,15 +26,23 @@ func (b *StringBuilder) PrintStruct(name string, v interface{}) string {
 		switch r.Kind() {
 		case reflect.String:
 			if v.(string) != "" {
-				b.str.WriteString(fmt.Sprintf("%s: %s,", name, v))
+				b.str.WriteString(fmt.Sprintf("%s: %s, ", name, v))
 			}
 		case reflect.Bool:
 			b.str.WriteString(fmt.Sprintf("%s: %t, ", name, v))
+		case reflect.Int:
+			b.str.WriteString(fmt.Sprintf("%s: %d, ", name, v))
 		case reflect.Ptr:
 			if !r.IsNil() {
 				fields := structs.Fields(v)
+				var printName string
 				for _, f := range fields {
-					b.PrintStruct(f.Name(), f.Value())
+					if name == "config" {
+						printName = f.Name()
+					} else {
+						printName = fmt.Sprintf("%s.%s", name, f.Name())
+					}
+					b.PrintStruct(printName, f.Value())
 				}
 			}
 		}
